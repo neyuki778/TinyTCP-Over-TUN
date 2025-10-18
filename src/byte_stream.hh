7f2b903ce17b4e75ec9cc054e08a6c806e2d7611
neyuki778 <2597605722@qq.com>
2025-10-18 23:38:13 +0800

    fix: para order

--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -27,10 +27,10 @@ protected:
   bool error_ {};
   // basic para cloud be used
   std::string buffer_;
-  bool closed_;
   // hinted by func
   uint64_t total_pushed_;
   uint64_t total_poped_;
+  bool closed_;
 };
 
 class Writer : public ByteStream

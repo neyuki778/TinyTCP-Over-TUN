93503ea1794e77842d2e6ec5178b4b11d95fe781
neyuki778 <2597605722@qq.com>
2025-10-19 13:54:16 +0800

    fix: format code and add a helper

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -15,6 +15,13 @@ void try_close(bool recv, uint64_t idx, Writer& writer);
 
 void update_first_unassembled_index_(uint64_t& idx, Reader& reader);
 
+void try_push_assembled_bytes(std::vector<char>& unassembled_bytes,
+                               std::vector<bool>& unassembled_present,
+                               uint64_t& first_unassembled_index_,
+                               std::string& data_to_push,
+                               Writer& writer,
+                               Reader& reader);
+
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
   // debug( "unimplemented insert({}, {}, {}) called", first_index, data, is_last_substring );
@@ -40,48 +47,42 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   //case0: stream index is too big
   if ( first_index > first_unacceptable_index_) return;
 
-  // bool pushed = false;
-
-  // case1: all data is legal
-  if ( first_index == first_unassembled_index_ ) {
-    // writer.push( data );
-    // first_unpopped_index_ += data.length();
-    if (!unassembled_bytes.empty()){
-      uint64_t first_index_in_unassembled_bytes = last_index - first_unassembled_index_;
-      auto it = first_index_in_unassembled_bytes;
-      for (; it < unassembled_bytes.size(); it++){
-        if (unassembled_present[it]){
-          data += unassembled_bytes[it];
-        }else{
-          break;
-        }
-      }
-      unassembled_bytes.erase(unassembled_bytes.begin(), unassembled_bytes.begin() + it);
-      unassembled_present.erase(unassembled_present.begin(), unassembled_present.begin() + it);
-      // unassembled_base_index_ += it;
-      update_first_unassembled_index_(first_unassembled_index_, reader);
-    }
-    writer.push(data);
-    // pushed = true;
-    try_close(eof_received_, eof_index_, writer);
-  }
-
-  // case2: data is longer than popped
-  if ( first_index < first_unassembled_index_ ){
-    writer.push( data.substr(first_unassembled_index_ - first_index ));
-    // pushed = true;
-    try_close(eof_received_, eof_index_, writer);
-  }
-
-  // case3: data should be stored in ressembler, suggested that first_index > first_unassembled_index_
-  uint64_t start_index = first_index - first_unassembled_index_;
-  if (unassembled_bytes.empty()) {
+  // init reassembler if need
+  if ( unassembled_bytes.empty() && available_capacity > 0 ) {
     unassembled_bytes.resize(available_capacity);
     unassembled_present.assign(available_capacity, false);
     // unassembled_base_index_ += first_unassembled_index_;
     update_first_unassembled_index_(first_unassembled_index_, reader);
-
   }
+
+  // bool pushed = false;
+
+  // case1: all data is legal
+  if ( first_index == first_unassembled_index_ ) {
+    string data_to_push = data;
+
+    try_push_assembled_bytes(unassembled_bytes, unassembled_present,
+                              first_unassembled_index_, data_to_push, writer, reader);
+
+    try_close(eof_received_, eof_index_, writer);
+    return;
+  }
+
+  // case2: data is longer than popped
+  if ( first_index < first_unassembled_index_ ){
+    // writer.push( data.substr(first_unassembled_index_ - first_index ));
+    uint64_t skip_len = first_unassembled_index_ -first_index;
+    string data_to_push = data.substr(skip_len);
+
+    try_push_assembled_bytes(unassembled_bytes, unassembled_present,
+                              first_unassembled_index_, data_to_push, writer, reader);
+
+    try_close(eof_received_, eof_index_, writer);
+    return;
+  }
+
+  // case3: data should be stored in ressembler, suggested that first_index > first_unassembled_index_
+  uint64_t start_index = first_index - first_unassembled_index_;
   // only store when the data lies beyond the current assembled prefix
   if (first_index > first_unassembled_index_) {
     place_string_efficiently(unassembled_bytes, unassembled_present, data, start_index);
@@ -120,6 +121,42 @@ void try_close(bool recv, uint64_t idx, Writer& writer){
 void update_first_unassembled_index_(uint64_t& idx, Reader& reader){
   idx = reader.bytes_buffered() + reader.bytes_popped();
 }
+
+void try_push_assembled_bytes(std::vector<char>& unassembled_bytes,
+                               std::vector<bool>& unassembled_present,
+                               uint64_t& first_unassembled_index_,
+                               std::string& data_to_push,
+                               Writer& writer,
+                               Reader& reader)
+{
+  if (unassembled_bytes.empty()) {
+    return;
+  }
+
+  // 从 unassembled_bytes 的起始位置开始查找连续的数据
+  uint64_t it = 0;
+  for (; it < unassembled_bytes.size(); it++) {
+    if (unassembled_present[it]) {
+      data_to_push += unassembled_bytes[it];
+    } else {
+      break;
+    }
+  }
+
+  // 如果找到了连续数据，清理已使用的部分
+  if (it > 0) {
+    unassembled_bytes.erase(unassembled_bytes.begin(), unassembled_bytes.begin() + it);
+    unassembled_present.erase(unassembled_present.begin(), unassembled_present.begin() + it);
+    update_first_unassembled_index_(first_unassembled_index_, reader);
+  }
+
+  // 推送所有累积的数据
+  if (!data_to_push.empty()) {
+    writer.push(data_to_push);
+    data_to_push.clear();
+  }
+}
+
 // How many bytes are stored in the Reassembler itself?
 // This function is for testing only; don't add extra state to support it.
 uint64_t Reassembler::count_bytes_pending() const

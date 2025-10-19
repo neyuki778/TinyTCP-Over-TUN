773b8a4b498c04d7ff1cdc93306a8bc606404042
neyuki778 <2597605722@qq.com>
2025-10-19 23:41:09 +0800

    feat: implement wrap and unwrap func

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -6,13 +6,20 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return zero_point + static_cast<uint32_t>( n );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  uint32_t offset = raw_value_ - zero_point.raw_value_;
+  uint64_t candidate = offset + ( checkpoint & 0xFFFFFFFF00000000 );
+
+  const uint64_t TWO_POW_32 = 1ULL << 32;
+  const uint64_t TWO_POW_31 = 1ULL << 31;
+
+  if ( candidate > checkpoint and candidate - checkpoint > TWO_POW_31) return candidate - TWO_POW_32;
+  if ( candidate < checkpoint and checkpoint - candidate > TWO_POW_31) return candidate + TWO_POW_32;
+
+  return candidate;
 }

92c9330b0fbf18cd7f68b7bb0439be3e385d2f75
neyuki778 <2597605722@qq.com>
2025-10-21 20:50:44 +0800

    feat: try implement push func whitout TOR

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,6 +1,7 @@
 #include "tcp_sender.hh"
 #include "debug.hh"
 #include "tcp_config.hh"
+#include <string_view>
 
 using namespace std;
 
@@ -20,8 +21,34 @@ uint64_t TCPSender::consecutive_retransmissions() const
 
 void TCPSender::push( const TransmitFunction& transmit )
 {
-  debug( "unimplemented push() called" );
-  (void)transmit;
+  TCPSenderMessage msg = make_empty_message();
+  if (!window_size_) {
+    transmit(msg);
+    return;
+  }
+  string_view payload_view = reader().peek();
+  // SYN
+  if (!syn_sent_){
+    syn_sent_ = true;
+    msg.SYN = true;
+  }
+  // FIN
+  if (reader().is_finished()){
+    msg.FIN = true;
+    fin_sent_ = true;
+  }
+  // RST
+  if (writer().has_error()){
+    msg.RST = true;
+  }
+  // payload
+  string payload(payload_view.substr(0, window_size_ - msg.sequence_length()));
+  msg.payload = payload;
+  next_ackno_ += msg.sequence_length();
+  // seqno
+  msg.seqno = Wrap32::wrap(next_ackno_, isn_);
+
+  transmit(msg);
 }
 
 TCPSenderMessage TCPSender::make_empty_message() const
@@ -33,6 +60,7 @@ TCPSenderMessage TCPSender::make_empty_message() const
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
   window_size_ = msg.window_size;
+  // next_ackno_ ?
   ackno_ = msg.ackno->unwrap(isn_, next_ackno_);
   if (msg.RST) writer().set_error();
 }

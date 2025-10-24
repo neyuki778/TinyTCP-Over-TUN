fd93ba0454376d7417ba0f5a5e5dfd425afb2089
neyuki778 <2597605722@qq.com>
2025-10-24 17:09:34 +0800

    fix

--- apps/webget.cc
+++ apps/webget.cc
@@ -27,9 +27,9 @@ void get_URL( const string& host, const string& path )
   string response;
   while ( !socket.eof() ) {
     socket.read( response );
-    cout << response;
   }
-  // socket.close();
+  cout << response;
+  // socket.wait_until_closed();
 }
 } // namespace
 

53bc18fddd156058bd029e94930be3855ea83203
neyuki778 <2597605722@qq.com>
2025-11-07 17:45:54 +0800

    fix

--- src/network_interface.cc
+++ src/network_interface.cc
@@ -104,7 +104,7 @@ void NetworkInterface::recv_frame( EthernetFrame frame )
     if (pending_ip_datagrams_.count(ip)){
       auto& pending_queue = pending_ip_datagrams_.at( ip );
       while ( !pending_queue.empty() ) {
-        InternetDatagram pending_dgram = pending_queue.back();
+        InternetDatagram pending_dgram = pending_queue.front();
         pending_queue.pop();
         send_datagram( pending_dgram, Address::from_ipv4_numeric( ip ) );
       }
